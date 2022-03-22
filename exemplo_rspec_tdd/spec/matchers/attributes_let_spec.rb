2bd7806c9f62e37ac39c9bf67c2d2a5cef9e0306
Henrique <hfvmarques@gmail.com>
2022-03-21 21:38:27 -0400

    hooks

---
+++ exemplo_rspec_tdd/spec/matchers/attributes_let_spec.rb
@@ -0,0 +1,25 @@
+require 'person'
+
+describe 'Attributes' do
+  # before(:each) do
+  #   puts 'ANTES DE CADA TESTE'
+  #   @person = Person.new
+  # end
+
+  let(:person) { Person.new }
+
+  it 'have_attributes' do
+    person.name = 'Henrique'
+    person.age = 34
+
+    expect(person).to have_attributes(name: 'Henrique', age: 34)
+    expect(person).to have_attributes(name: starting_with('H'), age: be >= 30)
+  end
+
+  it 'have_attributes' do
+    person.name = 'Hernandes'
+    person.age = 31
+
+    expect(person).to have_attributes(name: starting_with('H'), age: be >= 30)
+  end
+end
