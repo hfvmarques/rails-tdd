ec5d92613dafc202e6dd5a35015dd4bb190f3817
Henrique <hfvmarques@gmail.com>
2022-03-21 21:38:41 -0400

    let

---
+++ exemplo_rspec_tdd/spec/matchers/let_spec.rb
@@ -0,0 +1,14 @@
+$counter = 0
+
+describe 'let' do
+  let(:count) { $counter += 1 }
+
+  it 'memorize value' do
+    expect(count).to eq(1)
+    expect(count).to eq(1) # fica em cache dentro do mesmo teste
+  end
+
+  it 'is not cached between tests' do
+    expect(count).to eq(2)
+  end
+end
