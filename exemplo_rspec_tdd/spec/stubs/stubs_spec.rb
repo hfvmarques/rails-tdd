99f7e441aab6fb94d177b0990934481521dfc69b
Henrique <hfvmarques@gmail.com>
2022-03-29 16:42:17 -0400

    stubs methods

--- exemplo_rspec_tdd/spec/stubs/stubs_spec.rb
+++ exemplo_rspec_tdd/spec/stubs/stubs_spec.rb
@@ -12,4 +12,37 @@ describe 'Stub' do
 
     expect(course_finished).to be true
   end
+
+  it 'Dinamic Args' do
+    student = Student.new
+
+    allow(student).to receive(:foo) do |arg|
+      if arg == :bar
+        'bar'
+      else
+        'default'
+      end
+    end
+
+    expect(student.foo(:bar)).to eq('bar')
+    expect(student.foo(:baz)).to eq('default')
+  end
+
+  it 'Any instance of a class' do
+    student = Student.new
+    other_student = Student.new
+
+    allow_any_instance_of(Student).to receive(:bar).and_return(true)
+
+    expect(student.bar).to be true
+    expect(other_student.bar).to be true
+  end
+
+  it 'Errors' do
+    student = Student.new
+
+    allow(student).to receive(:bar).and_raise(RuntimeError)
+
+    expect { student.bar }.to raise_error(RuntimeError)
+  end
 end
