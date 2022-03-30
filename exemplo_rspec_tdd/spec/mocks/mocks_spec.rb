c161e66fa0665282b3043b2b05af9bb1c65e95f9
Henrique <hfvmarques@gmail.com>
2022-03-29 20:26:37 -0400

    mock expectations

--- exemplo_rspec_tdd/spec/mocks/mocks_spec.rb
+++ exemplo_rspec_tdd/spec/mocks/mocks_spec.rb
@@ -6,7 +6,42 @@ describe 'Mocks' do
     student = Student.new
     
     expect(student).to receive(:bar)
-    
+
     student.bar
+  end
+
+  it 'Args' do
+    student = Student.new
+    
+    expect(student).to receive(:foo).with(123)
+
+    student.foo(123)
+  end
+
+  it 'Repetition' do
+    student = Student.new
+    
+    expect(student).to receive(:foo).with(123).twice
+
+    student.foo(123)
+    student.foo(123)
+  end
+
+  it 'Repetition2' do
+    student = Student.new
+    
+    expect(student).to receive(:foo).with(123).at_least(2).times
+
+    student.foo(123)
+    student.foo(123)
+    student.foo(123)
+  end
+
+  it 'Return' do
+    student = Student.new
+    
+    expect(student).to receive(:foo).with(123).and_return(true)
+
+    student.foo(123)
   end
 end
